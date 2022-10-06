d1f903dff6932c1a7737da20529aa13ad84c151f
Danil Melnikov <damillmel29@gmail.com>
2022-10-06 11:20:40 +0300

    Add is leap year program

---
+++ Datetime.pas
@@ -0,0 +1,12 @@
+﻿{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}
+
+begin
+  var year := Readinteger;
+  if ((year mod 4 = 0) and ( (year mod 400 = 0) or (year mod 10 <> 0))) then
+  Println('год высокостен')
+  else 
+    println('Год не высокостен');
+  
+
+    
+end.
